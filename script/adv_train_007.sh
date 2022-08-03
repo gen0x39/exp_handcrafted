55cea0389f85f474b67cc01143965f03d8dfddf7
gen0x39 <migesan0319@gmail.com>
2022-08-03 11:58:43 +0900

    Add : test script

---
+++ script/adv_train_007.sh
@@ -0,0 +1,6 @@
+nohup python3 ../src/adv_train.py --batch_size 64 --gpu 0 --epochs 200 --adv_loss pgd --arch 130 &
+nohup python3 ../src/adv_train.py --batch_size 64 --gpu 0 --epochs 200 --adv_loss pgd --arch 131 &
+nohup python3 ../src/adv_train.py --batch_size 64 --gpu 0 --epochs 200 --adv_loss pgd --arch 132 &
+nohup python3 ../src/adv_train.py --batch_size 64 --gpu 0 --epochs 200 --adv_loss pgd --arch 133 &
+nohup python3 ../src/adv_train.py --batch_size 64 --gpu 0 --epochs 200 --adv_loss pgd --arch 200 &
+nohup python3 ../src/adv_train.py --batch_size 64 --gpu 0 --epochs 200 --adv_loss pgd --arch 201 &
