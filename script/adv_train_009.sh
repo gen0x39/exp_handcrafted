9931746051a5af13224f153f12f2a89266076a6e
gen0x39 <migesan0319@gmail.com>
2022-08-03 14:02:19 +0900

    Fix: script

---
+++ script/adv_train_009.sh
@@ -0,0 +1,10 @@
+nohup python3 ../src/adv_train.py --batch_size 64 --gpu 0 --epochs 200 --adv_loss pgd --arch 210 &
+nohup python3 ../src/adv_train.py --batch_size 64 --gpu 0 --epochs 200 --adv_loss pgd --arch 211 &
+nohup python3 ../src/adv_train.py --batch_size 64 --gpu 0 --epochs 200 --adv_loss pgd --arch 212 &
+nohup python3 ../src/adv_train.py --batch_size 64 --gpu 0 --epochs 200 --adv_loss pgd --arch 213 &
+nohup python3 ../src/adv_train.py --batch_size 64 --gpu 0 --epochs 200 --adv_loss pgd --arch 220 &
+nohup python3 ../src/adv_train.py --batch_size 64 --gpu 0 --epochs 200 --adv_loss pgd --arch 221 &
+nohup python3 ../src/adv_train.py --batch_size 64 --gpu 0 --epochs 200 --adv_loss pgd --arch 223 &
+nohup python3 ../src/adv_train.py --batch_size 64 --gpu 0 --epochs 200 --adv_loss pgd --arch 230 &
+nohup python3 ../src/adv_train.py --batch_size 64 --gpu 0 --epochs 200 --adv_loss pgd --arch 231 &
+nohup python3 ../src/adv_train.py --batch_size 64 --gpu 0 --epochs 200 --adv_loss pgd --arch 232 &
