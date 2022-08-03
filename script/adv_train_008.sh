50d04839fd9d298cb925a2073283928d1d8ea717
gen0x39 <migesan0319@gmail.com>
2022-08-03 12:20:00 +0900

    Fix: test script

---
+++ script/adv_train_008.sh
@@ -0,0 +1,6 @@
+nohup python3 ../src/adv_train.py --batch_size 64 --gpu 1 --epochs 200 --adv_loss pgd --arch 202 &
+nohup python3 ../src/adv_train.py --batch_size 64 --gpu 1 --epochs 200 --adv_loss pgd --arch 203 &
+nohup python3 ../src/adv_train.py --batch_size 64 --gpu 1 --epochs 200 --adv_loss pgd --arch 210 &
+nohup python3 ../src/adv_train.py --batch_size 64 --gpu 1 --epochs 200 --adv_loss pgd --arch 211 &
+nohup python3 ../src/adv_train.py --batch_size 64 --gpu 1 --epochs 200 --adv_loss pgd --arch 212 &
+nohup python3 ../src/adv_train.py --batch_size 64 --gpu 1 --epochs 200 --adv_loss pgd --arch 213 &
